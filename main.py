b1ffa8f3f3b9a2b8ca48f86903f2f52b5002f65d
David <davidgdalevich7@gmail.com>
2022-07-14 13:25:40 +0300

    WHEEEEEEEEEP

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+
+import pyautogui
+
+# Screen size
+print(pyautogui.size())
+
+# Mouse position
+print(pyautogui.position())
+
+# Finding a window and its location and wait until it's active
+window = []
+while len(window) == 0 or not window[0].isActive:
+    window = pyautogui.getWindowsWithTitle('Untitled - Notepad')
+    time.sleep(0.5)
+window = window[0]
+
+
+pyautogui.moveTo(window.centerx, window.centery)
+time.sleep(0.5)
+pyautogui.click()
+time.sleep(0.5)
+pyautogui.typewrite("I AM TEXT BITCH")
+
