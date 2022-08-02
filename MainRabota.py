d70ccc6b443dd49e848205892ec759f8a40f4393
Shlomo <shlomogdd@gmail.com>
2022-08-02 22:26:47 +0300

    finished mouse

--- MainRabota.py
+++ MainRabota.py
@@ -9,29 +9,52 @@ def random_range(min, max):
     num1 += min
     return num1
 
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
+
 screen_size_x, screen_size_y = pyautogui.size()
 
 # moves the mouse from where it is to Destination
 # gets: the destination x and destination y
 # returns: nothing
+
+ARRIVAL_DISTANCE = 10
+ANGLE_MAX_ERROR = 0.2
+ANGLE_CURVE = 0.15
+MOUSE_SPEED_MIN = screen_size_x / 500
+MOUSE_SPEED_MAX = screen_size_x / 400
+
+
 def mouse(destination_x, destination_y):
-    x,y = pyautogui.position() #current position
+    mouse_speed_min = MOUSE_SPEED_MIN
+    mouse_speed_max = MOUSE_SPEED_MAX
+    x_start,y_start = pyautogui.position() #current position
+    x, y = pyautogui.position()  # current position
+    angle_curve = ANGLE_CURVE * (1 if random.random() > 0.5 else -1)
     while x != destination_x and y != destination_y:
         angle = math.atan2(destination_x - x,destination_y - y) # angle randomizing:
-        angle_random = random_range(-0.2, 0.2)
-        angle = angle + angle_random + 0.2
-        mouse_move = random_range(screen_size_x / 100,screen_size_x / 50) # movement randomizing:
+        angle_random = random_range(-ANGLE_MAX_ERROR, ANGLE_MAX_ERROR)
+        angle = angle + angle_random + angle_curve
+
+        if mouse_speed_min > screen_size_x / 800:
+            if distance(x, y, x_start, y_start) < distance(x, y, destination_x, destination_y):
+                mouse_speed_min += screen_size_x / 1000
+                mouse_speed_max += screen_size_x / 1000
+            else:
+                mouse_speed_min -= screen_size_x / 1000
+                mouse_speed_max -= screen_size_x / 1000
+        mouse_move = random_range(mouse_speed_min ,mouse_speed_max) # movement randomizing:
 
         x += int(math.sin(angle) * mouse_move)
         y += int(math.cos(angle) * mouse_move)
-        pyautogui.moveTo(x, y) # moves with angle to Destination
+        pyautogui.moveTo(x, y, _pause=False) # moves with angle to Destination
 
-        if(-10 < destination_x - x < 10 and -10 < destination_y - y < 10): # jumps to the end when close:
+        if(abs(destination_x - x) < ARRIVAL_DISTANCE and abs(destination_y - y) < ARRIVAL_DISTANCE): # jumps to the end when close:
             pyautogui.moveTo(destination_x, destination_y)
             x = destination_x
             y = destination_y
 
-
-    print(pyautogui.position())
+        mouse_pause = random_range(0.005, 0.02)
+        time.sleep(mouse_pause)
 
 mouse(1000, 1000)
--- main.py
+++ main.py
@@ -12,18 +12,18 @@ print(pyautogui.size())
 print(pyautogui.position())
 
 # Finding a window and its location and wait until it's active
-window = []
-while len(window) == 0 or not window[0].isActive:
-    window = pyautogui.getWindowsWithTitle('Untitled - Notepad')
-    time.sleep(0.5)
-window = window[0]
-
-# Moves to center of the notepad and types some texts
-pyautogui.moveTo(window.centerx, window.centery)
-time.sleep(0.5)
-pyautogui.click()
-time.sleep(0.5)
-pyautogui.typewrite("I AM TEXT BITCH")
+# window = []
+# while len(window) == 0 or not window[0].isActive:
+#     window = pyautogui.getWindowsWithTitle('Untitled - Notepad')
+#     time.sleep(0.5)
+# window = window[0]
+#
+# # Moves to center of the notepad and types some texts
+# pyautogui.moveTo(window.centerx, window.centery)
+# time.sleep(0.5)
+# pyautogui.click()
+# time.sleep(0.5)
+# pyautogui.typewrite("I AM TEXT BITCH")
 
 # Now open calculator
 pyautogui.keyDown('winleft')
