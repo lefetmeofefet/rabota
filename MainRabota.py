17abf94f0e5843617926db33292a5c4b48ac7da9
Shlomo <shlomogdd@gmail.com>
2022-08-04 14:07:42 +0300

    fixed som,e small code

--- MainRabota.py
+++ MainRabota.py
@@ -25,28 +25,23 @@ def enter_game():
         create_window = True
 
     while not create_window:
-        create_button_unpressed = pyautogui.locateCenterOnScreen(images_folder + "create_unpressed.png",)
-        if create_button_unpressed is not None:
-            utytilities.move_mouse(create_button_unpressed.x, create_button_unpressed.y)
-            time.sleep(0.2)
-            utytilities.mouse_click()
-            create_button_pressed = pyautogui.locateCenterOnScreen(images_folder + "create_pressed.png")
-            if create_button_pressed is not None:
-                create_window = True
-        time.sleep(0.4)
+        create_button_unpressed = utytilities.wait_until_found(images_folder + "create_unpressed.png",)
+        utytilities.move_mouse(create_button_unpressed.x, create_button_unpressed.y)
+        time.sleep(0.2)
+        utytilities.mouse_click()
 
-    #write the game name:z
-    game_name = pyautogui.locateCenterOnScreen(images_folder + "game_name.png")
+    #write the game name: (starts with countess1 always)
+    game_name = utytilities.wait_until_found(images_folder + "game_name.png")
     utytilities.move_mouse(game_name.x + utytilities.random_range(-2, 6), game_name.y + 25 * utytilities.MOUSE_SPEED_ACCELERATION)
     utytilities.mouse_click()
     utytilities.backspace(10)
-    utytilities.mouse_click(utytilities.write_text(utytilities.create_run_name()))
+    utytilities.write_text(utytilities.create_run_name())
 
     #add password if we want!!
     # add password if we want!!
     # add password if we want!!
 
-    create_game_button = pyautogui.locateCenterOnScreen(images_folder + "create_game_button.png")
+    create_game_button = utytilities.wait_until_found(images_folder + "create_game_button.png")
     utytilities.move_mouse(create_game_button.x, create_game_button.y)
     utytilities.mouse_click()
 
