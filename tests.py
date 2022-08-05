f59f06934c5804fa4989485ce957da66aa61a0ee
Numbnut <davidgdalevich7@gmail.com>
2022-08-05 19:22:08 +0300

    fklerp

--- tests.py
+++ tests.py
@@ -8,10 +8,36 @@ class Point:
         self.y = y
 
 
+
+def test():
+    screenshot = pyautogui.screenshot()
+
+    wall_colors1 = [
+        (100, 100, 100),
+        (100, 90, 79),
+    ]
+    wall_colors2 = [
+        (66, 61, 48),
+
+    ]
+
+    # Maybe for crossroads indicator (11, 1, 0),
+    # Maybe (74, 68, 63),
+
+    # screenshot.remap_palette()
+    for x in range(screenshot.width):
+        for y in range(screenshot.height):
+            pixel = screenshot.getpixel((x, y))
+            if pixel in wall_colors1:
+                screenshot.putpixel((x, y), (255, 0, 0))
+            elif pixel in wall_colors2:
+                screenshot.putpixel((x, y), (0, 255, 0))
+            else:
+                # screenshot.putpixel((x, y), (0, 0, 0))
+                screenshot.putpixel((x, y), (pixel[0] // 4, pixel[1] // 4, pixel[2] // 4))
+    screenshot.save("test.png")
+
+
+
 time.sleep(1)
-wp = find_wp_on_minimap()
-if wp is None:
-    print("Shit")
-if wp is not None:
-    print(wp.x, wp.y)
-    pyautogui.moveTo(wp.x, wp.y)
+test()
